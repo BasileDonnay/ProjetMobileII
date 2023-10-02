c2d4e45d9855037954b8f0c32171368dd8fa74a6
Base <bdonnay@helb-prigogine.be>
2023-10-02 13:38:02 +0200

    repaired on android

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,41 +1,8 @@
-/*import React, { useState } from 'react';
-import { View, Text, TextInput} from 'react-native';
-//import { NavigationContainer } from '@react-navigation/native';
-//import { createStackNavigator } from '@react-navigation/stack';
-import { styles } from './Styles'; // Import the styles
-
-// ... your other imports ...
-
-function LevelScreen({ route, navigation }) {
-  const { level } = route.params;
-  const goBackToHome = () => {
-    navigation.goBack();
-  };
-
-  const [code, setCode] = useState(`function myFunction() {\n  console.log('Hello, World!');\n}`);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Level {level}</Text>
-      <TextInput
-        multiline
-        style={styles.codeEditor}
-        value={code}
-        onChangeText={(newCode) => setCode(newCode)}
-      />
-      <CustomButton title="Go Back" onPress={goBackToHome} />
-    </View>
-  );
-}
-
-// Export the LevelScreen function
-export default LevelScreen;*/
-
 import React from 'react';
 import { View, Text, ScrollView, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { styles } from './styles'; // Import the styles
-//import { CustomButton } from './CustomButton';
+import {styles} from './styles/Styles'; // Import the styles
+import CustomButton from './components/CustomButton';
 
 function HomeScreen() {
   const navigation = useNavigation();
@@ -61,8 +28,8 @@ function HomeScreen() {
   );
 }
 
-function CustomButton({ title, onPress }) {
+/*function CustomButton({ title, onPress }) {
   return <Button title={title} onPress={onPress} />;
-}
+}*/
 
 export default HomeScreen;
